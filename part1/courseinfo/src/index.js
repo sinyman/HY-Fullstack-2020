ab8e4c90f2fa84da139f2176f43fb7bbf73bb404
Simon Nyman <nyman.simon@hotmail.com>
2019-09-27 17:00:52 +0300

    Exercise 1.2 done

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -13,9 +13,9 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content part1={part1} exercises1={exercises1}
+        part2={part2} exercises2={exercises2}
+        part3={part3} exercises3={exercises3} />
       <Total exercises={exercises1 + exercises2 + exercises3} />
     </div>
   )
@@ -30,7 +30,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <p>
-      {props.part}<br/> Exercises: {props.exercises}
+      {props.part1}<br/> Exercises: {props.exercises1}<br/><br/>
+      {props.part2}<br/> Exercises: {props.exercises2}<br/><br/>
+      {props.part3}<br/> Exercises: {props.exercises3}
     </p>
   )
 }
