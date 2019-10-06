a5092294e1b58a7fa3c03c45da9cf38d9b5df0ca
Simon Nyman <nyman.simon@hotmail.com>
2019-10-06 20:44:07 +0300

    Exercises 1.12-14 done

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,18 +1,52 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const TopAnecdote = ({votes}) => {
+  let largest = votes.indexOf(Math.max(...votes));
+
+  if(votes[largest] === 0) {
+    return(
+      <div>
+        <h2>Anecdote with the most votes</h2>
+        Insufficient amount of votes to determine a winner
+      </div>
+    )
+  }
+  return(
+    <div>
+      <h2>Anecdote with the most votes</h2>
+      "{anecdotes[largest]}"<br/>
+      with {votes[largest]} votes!
+    </div>
+  )
+}
+
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+
 
   const setSelectedAnecdote = () => {
-    setSelected(Math.floor(Math.random() * anecdotes.length))
+    let random = Math.floor(Math.random() * anecdotes.length);
+    setSelected(random)
+  }
+
+  const addVote = () => {
+    let copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
   }
 
   return (
     <div>
-      {props.anecdotes[selected]}
+      <h2>Anecdote of the day</h2>
+      "{props.anecdotes[selected]}"
+      <br/>has {votes[selected]} votes
       <br/>
-      <button onClick={() => setSelectedAnecdote()}>Next</button>
+      <button onClick={() => setSelectedAnecdote()} >Next anecdote</button>
+      <button onClick={() => addVote()} >Vote</button>
+      <br/>
+      <TopAnecdote votes={votes} />
     </div>
   )
 }
@@ -23,7 +57,8 @@ const anecdotes = [
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+  'Live, love, mene töihin!'
 ]
 
 ReactDOM.render(
