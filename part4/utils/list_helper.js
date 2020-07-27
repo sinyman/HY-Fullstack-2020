6a2bdf6a34ef7c93a0a735804cfff20e6885d2d2
Simon Nyman <nyman.simon@hotmail.com>
2020-07-27 15:59:17 +0300

    Exercise 4.5 done

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -9,7 +9,21 @@ const totalLikes = (blogs) => {
   }, 0)
 }
 
+const favouriteBlog = (blogs) => {
+  return blogs.sort(comparator)[blogs.length-1]
+}
+
+const comparator = (a, b) => {
+  if(a.likes < b.likes) {
+    return -1
+  } else if (a.likes > b.likes) {
+    return 1
+  }
+  return 0
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog
 }
