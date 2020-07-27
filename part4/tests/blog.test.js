4ceba3ca84605b1a364e7e5f7804b574abe6d54d
Simon Nyman <nyman.simon@hotmail.com>
2020-07-27 20:27:07 +0300

    Exercise 4.6 done

--- part4/tests/blog.test.js
+++ part4/tests/blog.test.js
@@ -13,7 +13,7 @@ const blogs = [
   { _id: "5a422b3a1b54a676234d17f9", title: "Canonical string reduction", author: "Edsger W. Dijkstra", url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html", likes: 12, __v: 0 },
   { _id: "5a422b891b54a676234d17fa", title: "First class tests", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll", likes: 10, __v: 0 },
   { _id: "5a422ba71b54a676234d17fb", title: "TDD harms architecture", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html", likes: 0, __v: 0 },
-  { _id: "5a422bc61b54a676234d17fc", title: "Type wars", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html", likes: 2, __v: 0 }
+  { _id: "5a422bc61b54a676234d17fc", title: "Type wars", author: "Robert C. Martin", url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html", likes: 2, __v: 0 },
 ]
 
 describe('Total likes', () => {
@@ -46,7 +46,7 @@ describe('Favourite blog', () => {
 
   test('is the one with the most likes', () => {
     let fav = listHelper.favouriteBlog(blogs)
-    
+
     let actualFav = {
       _id: "5a422b3a1b54a676234d17f9",
       title: "Canonical string reduction",
@@ -58,5 +58,18 @@ describe('Favourite blog', () => {
 
     expect(fav).toEqual(actualFav)
 
+  })
+})
+
+describe('Biggest amount of blogs by author', () => {
+
+  test('is calculated correctly', () => {
+    let blogQuantity = listHelper.topAuthor(blogs).blogs
+    expect(blogQuantity).toBe(3)
+  })
+
+  test('corresponds to correct author', () => {
+    let returnedAuthor = listHelper.topAuthor(blogs).author
+    expect(returnedAuthor).toBe("Robert C. Martin")
   })
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,5 @@
+var _ = require('lodash');
+
 const dummy = (blogs) => {
   return 1;
 }
@@ -10,20 +12,44 @@ const totalLikes = (blogs) => {
 }
 
 const favouriteBlog = (blogs) => {
+
+  let comparator = (a, b) => {
+    if(a.likes < b.likes) {
+      return -1
+    } else if (a.likes > b.likes) {
+      return 1
+    }
+    return 0
+  }
+
   return blogs.sort(comparator)[blogs.length-1]
 }
 
-const comparator = (a, b) => {
-  if(a.likes < b.likes) {
-    return -1
-  } else if (a.likes > b.likes) {
-    return 1
-  }
-  return 0
+const topAuthor = (blogs) => {
+  let publications = _.groupBy(blogs, 'author')
+
+  let tmp = []
+
+  _.forEach(publications, (pubs, author) => {
+    tmp.push({ 'author': author, 'blogs': pubs.length, })
+  });
+
+  // Reverse order sort so person with most publications will be at tmp[0]
+  tmp.sort((a, b) => {
+    if(a.blogs < b.blogs) {
+      return 1
+    } else if (a.blogs > b.blogs) {
+      return -1
+    }
+    return 0
+  })
+
+  return tmp[0]
 }
 
 module.exports = {
   dummy,
   totalLikes,
-  favouriteBlog
+  favouriteBlog,
+  topAuthor
 }
