f71bf2facddf34bf9041130d9c810ed0f0ebe02f
Simon Nyman <nyman.simon@hotmail.com>
2020-12-21 14:56:00 +0200

    WIP commit exercise 5.2

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,14 +1,23 @@
 import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import Blog from './components/Blog'
 import LoginForm from './components/LoginForm'
+import BlogCreationForm from './components/BlogCreationForm'
+import Notification from './components/Notification'
+
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [blogTitle, setBlogTitle] = useState('')
+  const [blogAuthor, setBlogAuthor] = useState('')
+  const [blogURL, setBlogURL] = useState('')
+
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [notification, setNotification] = useState(null)
+
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedAppUser')
@@ -20,6 +29,7 @@ const App = () => {
     }
   }, [])
 
+  // -- LOGIN FUNCTIONALITY --
   const loginFormHandler = async (event) => {
     event.preventDefault()
 
@@ -34,20 +44,25 @@ const App = () => {
       window.localStorage.setItem(
         'loggedAppUser', JSON.stringify(user)
       )
+      blogService.setToken(user.token)
 
       blogService.getUsersBlogs(user)
       .then(blogs => setBlogs(blogs))
 
     } catch (exception) {
-      window.alert('Username or password invalid!')
+      //window.alert('Username or password invalid!')
+      setNotification('Error: Username or password invalid!')
+      setTimeout(() => {
+        setNotification(null)
+      }, 5000)
     }
   }
 
-  const handleChange = (event) => {
-    if(event.target.name === 'uname') {
-      setUsername(event.target.value)
+  const handleChange = ({ target }) => {
+    if(target.name === 'uname') {
+      setUsername(target.value)
     } else {
-      setPassword(event.target.value)
+      setPassword(target.value)
     }
   }
 
@@ -55,12 +70,61 @@ const App = () => {
     setUser(null)
     setBlogs([])
     window.localStorage.removeItem('loggedAppUser')
-
+    blogService.setToken('')
+    setNotification('')
   }
 
+  // -- BLOG ADDING FUNCTIONALITY --
+  const addBlog = (event) => {
+    event.preventDefault()
+
+    const newBlog = {
+      title: blogTitle,
+      author: blogAuthor,
+      url: blogURL
+    }
+
+    // TODO fix rerender problem så he tömber
+    // inputs
+
+    blogService
+    .create(newBlog)
+    .then(response => {
+      setBlogTitle('')
+      setBlogAuthor('')
+      setBlogURL('')
+
+      if(!response.isError) {
+        setBlogs(blogs.concat(response))
+        setNotification(`'${response.title}' by ${response.author} added!`)
+        setTimeout(() => {
+          setNotification(null)
+        }, 5000)
+
+      } else {
+        setNotification(`Error: ${response['message']}`)
+        setTimeout(() => {
+          setNotification(null)
+        }, 5000)
+      }
+    })
+  }
+
+  const handleBlogChange = ({ target }) => {
+    if(target.name === 'blogTitle') {
+      setBlogTitle(target.value)
+    } else if(target.name === 'blogAuthor') {
+      setBlogAuthor(target.value)
+    } else {
+      setBlogURL(target.value)
+    }
+  }
+
+  // -- RENDERING APP --
   if(!user) {
     return (
       <div>
+        <Notification message={notification} />
         <LoginForm formHandler={loginFormHandler} changeHandler={handleChange} />
       </div>
     )
@@ -68,13 +132,15 @@ const App = () => {
   return (
     <div>
       <button onClick={logout}>Log out</button>
+      <Notification message={notification} />
+      <BlogCreationForm submit={addBlog} changeHandler={handleBlogChange} />
       <h2>{user.name}'s blogs</h2>
       <ul>
-        {blogs.map(blog =>
+      {blogs.map(blog =>
         <li key={blog.id}>
-          <Blog blog={blog} />
+        <Blog blog={blog} />
         </li>
-        )}
+      )}
       </ul>
     </div>
   )
--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -2,6 +2,14 @@ import axios from 'axios'
 const baseUrl = '/api/blogs'
 const userUrl = '/api/users'
 
+let token = null
+
+// Called from main class
+// eslint-disable-next-line
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+}
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
@@ -17,4 +25,20 @@ const getUsersBlogs = (userToGet) => {
   })
 }
 
-export default { getAll, getUsersBlogs }
+const create = (blog) => {
+  const config = {
+    headers: {
+      'Authorization': token
+    }
+  }
+
+  let request = axios.post(baseUrl, blog, config)
+  return request.then(response => {
+    return {...response.data, isError: false }
+
+  }).catch(error => {
+    return {...error.response.data, isError: true }
+  })
+}
+
+export default { getAll, getUsersBlogs, create, setToken }
